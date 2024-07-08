23eaa51d5fd4891e71f6d229bb926fa312d237e9
lx78WyY0J5 <9j2k37st@gmail.com>
2024-07-08 14:05:59 +0200

    update script

--- note/OS/Windows/MV-Creator.ps1
+++ note/OS/Windows/MV-Creator.ps1
@@ -1,15 +1,17 @@
 [String]$VMName = "PS-VM-Test"
 [String]$VMPath = "C:\Users\Administrateur\Desktop\Hyper-V\"
-[String]$Switch = "NAT"
-[String]$SwitchBis = "Serveur"
+[String[]]$Switch = "NAT","Serveurs"
 [UInt64]$DiskSize = 50GB
 [UInt64]$RAMSize = 4GB
 [Int]$CPU = 2
-[String]$ISO = ""
+[String]$ISO = "C:\Users\Administrateur\Desktop\ISO\20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_fr-fr.iso"
 
-New-VM -name $VMName -MemoryStartupBytes $RAMSize -SwitchName $Switch -NewVHDPath $VMPath+$VMName+".VHDX" -NewVHDSizeBytes $DiskSize -Path $VMPath+$VMName -Version 10.0 -Generation 2 -Confirm:$true
+New-VM -name $VMName -MemoryStartupBytes $RAMSize -NewVHDPath $VMPath+$VMName+".VHDX" -NewVHDSizeBytes $DiskSize -Path $VMPath+$VMName -Version 10.0 -Generation 2 -Confirm:$true -BootDevice "VHD"
+# -SwitchName $Switch
 
-Add-VMNetworkAdapter -VMName $VMName -SwitchName $SwitchBis
+foreach ($SwitchBis in $Switch) {
+    Add-VMNetworkAdapter -VMName $VMName -SwitchName $SwitchBis
+}
 
 Set-VMProcessor –VMName $VMName –count $CPU
 
