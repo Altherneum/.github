94095c1d347e6f8e0fabba4583fd7c937a0534cb
lx78WyY0J5 <9j2k37st@gmail.com>
2024-07-08 14:34:21 +0200

    Update boot order

--- note/OS/Windows/MV-Creator.ps1
+++ note/OS/Windows/MV-Creator.ps1
@@ -1,15 +1,22 @@
+# Paths
 [String]$VMName = "PS-VM-Test"
 [String]$VMPath = "C:\Users\Administrateur\Desktop\Hyper-V\"
 [String]$VHDPath = $VMPath + $VMName + "\" + $VMName + ".VHDX"
 [String]$VMPathName = $VMPath + $VMName
+[String]$ISO = "C:\Users\Administrateur\Desktop\ISO\20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_fr-fr.iso"
+# Set VM configuration
 [String[]]$Switch = "NAT","Serveurs"
 [UInt64]$DiskSize = 50GB
 [UInt64]$RAMSize = 4GB
 [Int]$CPU = 2
-[String]$ISO = "C:\Users\Administrateur\Desktop\ISO\20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_fr-fr.iso"
+# Boot order
+$network = Get-VMNetworkAdapter -VMName $VMName
+$vhd = Get-VMHardDiskDrive -VMName $VMName
+$dvd = Get-VMDvdDrive -VMName $VMName
 
-New-VM -name $VMName -MemoryStartupBytes $RAMSize -NewVHDPath $VHDPath -NewVHDSizeBytes $DiskSize -Path $VMPathName -Version 10.0 -Generation 2 -Confirm:$true -BootDevice "VHD"
+New-VM -name $VMName -MemoryStartupBytes $RAMSize -NewVHDPath $VHDPath -NewVHDSizeBytes $DiskSize -Path $VMPathName -Version 10.0 -Generation 2 -Confirm:$true
 # -SwitchName $Switch
+# -BootDevice "CD"
 
 foreach ($SwitchBis in $Switch) {
     Add-VMNetworkAdapter -VMName $VMName -SwitchName $SwitchBis
@@ -18,3 +25,5 @@ foreach ($SwitchBis in $Switch) {
 Set-VMProcessor –VMName $VMName –count $CPU
 
 Add-VMDvdDrive -VMName $VMName -Path $ISO
+
+Set-VMFirmware -VMName $name -BootOrder $dvd,$network,$vhd
