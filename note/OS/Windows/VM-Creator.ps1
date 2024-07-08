cfee0bc67854ed26764b3d3c5ccb44743f081b12
lx78WyY0J5 <9j2k37st@gmail.com>
2024-07-08 14:54:48 +0200

    update

---
+++ note/OS/Windows/VM-Creator.ps1
@@ -0,0 +1,32 @@
+# Paths
+[String]$VMName = "PS-VM-Test"
+[String]$VMPath = "C:\Users\Administrateur\Desktop\Hyper-V\"
+[String]$VHDPath = $VMPath + $VMName + "\" + $VMName + ".VHDX"
+[String]$VMPathName = $VMPath + $VMName
+
+# Set VM configuration
+[String[]]$Switch = "NAT","Serveurs"
+[UInt64]$DiskSize = 50GB
+[UInt64]$RAMSize = 4GB
+
+New-VM -name $VMName -MemoryStartupBytes $RAMSize -NewVHDPath $VHDPath -NewVHDSizeBytes $DiskSize -Path $VMPathName -Version 10.0 -Generation 2 -Confirm:$true
+
+# NetWork Adapter
+foreach ($SwitchBis in $Switch) {
+    Add-VMNetworkAdapter -VMName $VMName -SwitchName $SwitchBis
+}
+
+# CPU
+[Int]$CPU = 2
+Set-VMProcessor –VMName $VMName –count $CPU
+
+# ISO
+[String]$ISO = "C:\Users\Administrateur\Desktop\ISO\20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_fr-fr.iso"
+Add-VMDvdDrive -VMName $VMName -Path $ISO
+
+# Boot order
+$dvd = Get-VMDvdDrive -VMName $VMName
+Set-VMFirmware -VMName $VMName -FirstBootDevice $dvd
+
+# Dynamic RAM
+Set-VMMemory $VMName -DynamicMemoryEnabled $true
