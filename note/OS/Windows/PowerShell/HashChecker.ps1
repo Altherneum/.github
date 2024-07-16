1f0e259b6b07b8fd810fb91a5f9e725fa257e249
lx78WyY0J5 <9j2k37st@gmail.com>
2024-07-16 21:11:31 +0200

    update UTF format

--- note/OS/Windows/PowerShell/HashChecker.ps1
+++ note/OS/Windows/PowerShell/HashChecker.ps1
@@ -1,11 +1,11 @@
-[String]$Hash = Read-Host "Entrez le Hash SHA256 à vérifier"
+﻿[String]$Hash = Read-Host "Entrez le Hash SHA256 à vérifier"
 [String]$Path = Read-Host "Entrez le chemin du fichier à vérifier"
 [String]$HashType = Read-Host "Entrez le type de Hash (Exemple SHA256)"
-Write-Host "La commande suivante va être lancée : Get-FileHash $Path -Algorithm $HashType | Select-Object -ExpandProperty 'Hash'"
+Write-Host "La commande suivante va être lancée :    Get-FileHash $Path -Algorithm $HashType | Select-Object -ExpandProperty 'Hash'    ..."
 [String]$Hashed = Get-FileHash $Path -Algorithm $HashType | Select-Object -ExpandProperty 'Hash'
 if($Hash -eq $Hashed){
-    Write-Host "🟢 Le Hash est identique ✅"
+    Write-Host "Le Hash est identique"
 }
 else {
-    Write-Host "🛑 Le Hash n'est pas identique ❌"
+    Write-Host "Le Hash n'est pas identique"
 }
