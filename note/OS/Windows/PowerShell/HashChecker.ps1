1be7d92921d2503508d116e3af169ba470cc8ce4
lx78WyY0J5 <9j2k37st@gmail.com>
2024-07-16 20:51:55 +0200

    remove : in double

--- note/OS/Windows/PowerShell/HashChecker.ps1
+++ note/OS/Windows/PowerShell/HashChecker.ps1
@@ -1,6 +1,6 @@
-[String]$Hash = Read-Host "Entrez le Hash SHA256 à vérifier : "
-[String]$Path = Read-Host "Entrez le chemin du fichier à vérifier : "
-[String]$HashType = Read-Host "Entrez le type de Hash (Exemple SHA256) : "
+[String]$Hash = Read-Host "Entrez le Hash SHA256 à vérifier"
+[String]$Path = Read-Host "Entrez le chemin du fichier à vérifier"
+[String]$HashType = Read-Host "Entrez le type de Hash (Exemple SHA256)"
 Write-Host "La commande suivante va être lancée : Get-FileHash $Path -Algorithm $HashType | Select-Object -ExpandProperty 'Hash'"
 [String]$Hashed = Get-FileHash $Path -Algorithm $HashType | Select-Object -ExpandProperty 'Hash'
 if($Hash -eq $Hashed){
