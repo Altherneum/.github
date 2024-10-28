34f77e1eb4458df00dbdd7afc81bc30ca165d369
lx78WyY0J5 <9j2k37st@gmail.com>
2024-10-28 15:39:47 +0100

    update to try new way

--- note/OS/Linux/Arch/arch-install-conf.sh
+++ note/OS/Linux/Arch/arch-install-conf.sh
@@ -1,15 +1,17 @@
 # Execute with :
 # curl -o /script.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/arch-install-conf.sh
 
+# Variables
 hostname=desktop
 username=admin
 password=password
+rootpassword=toor
 lvmpassword=lvmpassword
 device=/dev/sda
 
+# Setup
 loadkeys fr
 pacman -Syy
-timedatectl set-ntp true
 
 # Disques
 parted --script "${device}" -- mklabel gpt \
@@ -17,63 +19,58 @@ parted --script "${device}" -- mklabel gpt \
   set 1 boot on \
   mkpart primary ext4 129MiB 100%
 
-# Formater
+# Formater la partition /boot
 mkfs.fat -F32 /dev/sda1
 
 # Cryptlvm
-echo -n "${passwordlvm}" cryptsetup --use-random luksFormat /dev/sda2
-cryptsetup luksOpen /dev/sda2 cryptlvm
-#
-pvcreate /dev/mapper/cryptlvm
-vgcreate vg0 /dev/mapper/cryptlvm
+echo $lvmpassword | cryptsetup --use-random luksFormat /dev/sda2
+echo $lvmpassword | cryptsetup luksOpen /dev/sda2 cryptlvm
 
-# LV
-lvcreate --size 1G -n root vg0
-lvcreate --size 4G -n swap vg0
-lvcreate -l 100%FREE -n home vg0
-lvreduce --size -256M vg0/home
+# Formater la partition chiffré
+mkfs.ext4 /dev/mapper/cryptlvm
 
-# Formater
-mkswap /dev/vg0/swap
-mkfs.ext4 /dev/vg0/root
-mkfs.ext4 /dev/vg0/home
 # Mount
-mount /dev/sda3 /mnt
+mount /dev/mapper/cryptlvm /mnt
 mount --mkdir /dev/sda1 /mnt/boot
-mount --mkdir /dev/vg0/home /mnt/home
-swapon /dev/vg0/swap
 
-yes | pacstrap /mnt base linux linux-firmware
-genfstab -U /mnt >> /mnt/etc/fstab
+# Packages
+yes | pacstrap /mnt base linux linux-firmware base-devel openssh git sudo nano lvm2 cryptsetup grub efibootmgr networkmanager
 
-
-yes | pacstrap -K /mnt base linux linux-firmware openssh git sudo nano
-
+# Genfstab config
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Jump into installation
 arch-chroot /mnt /bin/bash
 
+# User config
+echo "KEYMAP=fr" > /etc/vconsole.conf
+echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
+timedatectl set-timezone Europe/Paris
+timedatectl set-ntp true
 
-# # User
-# useradd -m -s /bin/bash ${username}
-# passwd ${username} <<< ${password}
-# usermod -aG sudo ${username}
-# hostnamectl set-hostname ${hostname}
+# User
+echo "$rootpassword" | passwd --stdin
 
-# # mkinitcpio
-# sed -i 's/HOOKS=(.*)/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
-# mkinitcpio -P
+useradd -m -s /bin/bash ${username}
+echo "$password" | passwd $username --stdin
 
-# # Grub
-# pacman -S --noconfirm --needed grub efibootmgr 
-# sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"GRUB_CMDLINE_LINUX="cryptdevice=/dev/<your-disk-luks>:cryptlvm root=/dev/vg0/root"/' /etc/default/grub
-# #grub-install --target=x86_64-efi --efi-directory=/boot/efi --removable --recheck
-# #grub-install --target=x86_64-efi --bootloader-id=Arch --efi-directory=/boot/efi
+usermod -aG sudo ${username}
+### Vérifier le fichier sudoers si le groupe sudo est OK pour utiliser l'élévation de privilèges
+hostnamectl set-hostname ${hostname}
+
+# mkinitcpio
+sed -i 's/HOOKS=(.*)/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
+mkinitcpio -P
+
+# grub-install --target=x86_64-efi --bootloader-id=ArchLinux --efi-directory=/boot /dev/sda
+
+# UUIDcrypt=blkid -o value -s UUID /dev/sda2
+# UUIDroot=blkid -o value -s UUID /dev/mapper/cryptlvm
+# sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"GRUB_CMDLINE_LINUX="cryptdevice=UUID=$UUIDcrypt:cryptlvm root=UUID=$UUIDroot"/' /etc/default/grub
+
 # grub-mkconfig -o /boot/grub/grub.cfg
 
+# systemctl enable NetworkManager
 
-# # User config
-# echo "KEYMAP=fr" > /etc/vconsole.conf
-# echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
-# timedatectl set-timezone Europe/Paris
+# exit
+# reboot
