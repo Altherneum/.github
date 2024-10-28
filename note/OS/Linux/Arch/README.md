e635270b06ec5fc7cfe95a4f40d34745c3ba9879
lx78WyY0J5 <9j2k37st@gmail.com>
2024-10-28 23:22:34 +0100

    update arch script

---
+++ note/OS/Linux/Arch/README.md
@@ -0,0 +1,10 @@
+# Execute arch install
+## Keyboard
+`loadkeys fr` to load French keyboard "AZERTY"
+## Downloading and running script
+```
+curl -o /script.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/arch.sh
+nano /script.sh
+chmod +x /script.sh
+/script.sh
+```
--- note/OS/Linux/Arch/post-install.sh
+++ note/OS/Linux/Arch/post-install.sh
@@ -1,56 +1,8 @@
-# # Execute with :
-# loadkeys fr
-# curl -o /script.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/arch-install-conf.sh
-# nano /script.sh
-# chmod +x /script.sh
-# /script.sh
-
 # Variables
 hostname=desktop
 username=admin
 password=password
 rootpassword=toor
-lvmpassword=lvmpassword
-device=/dev/sda
-
-# Setup
-loadkeys fr
-pacman -Syy
-
-# Disques
-parted --script "${device}" -- mklabel gpt \
-  mkpart ESP fat32 1Mib 257MiB \
-  set 1 boot on \
-  mkpart primary ext4 257MiB 100%
-
-# Formater la partition /boot
-mkfs.fat -F32 /dev/sda1
-
-# Cryptlvm
-echo $lvmpassword | cryptsetup --use-random luksFormat /dev/sda2
-echo $lvmpassword | cryptsetup luksOpen /dev/sda2 cryptlvm
-
-# Formater la partition chiffré
-mkfs.ext4 /dev/mapper/cryptlvm
-
-# Mount
-mount /dev/mapper/cryptlvm /mnt
-mount --mkdir /dev/sda1 /mnt/boot
-# TEST
-# mount /dev/sdaN /mnt
-mount --rbind /dev  /mnt/dev
-mount --rbind /proc /mnt/proc
-mount --rbind /sys  /mnt/sys
-# TEST
-
-# Packages
-yes | pacstrap /mnt base linux linux-firmware base-devel lvm2 cryptsetup grub efibootmgr networkmanager
-# Removed for test download speed # yes | pacstrap openssh git sudo nano
-# Genfstab config
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Jump into installation
-arch-chroot /mnt /bin/bash <<"EOT"
 
 # User config
 echo "KEYMAP=fr" > /etc/vconsole.conf
@@ -85,4 +37,3 @@ systemctl enable NetworkManager
 
 # exit
 # reboot
-EOT
