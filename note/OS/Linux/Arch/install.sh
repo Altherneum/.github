994c4d23122dc975c50198ad174c9949a9d36e2c
lx78WyY0J5 <9j2k37st@gmail.com>
2025-02-14 12:03:22 +0100

    split file to test new arch-chroot way

--- note/OS/Linux/Arch/install.sh
+++ note/OS/Linux/Arch/install.sh
@@ -57,60 +57,12 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 read -p "Press [Enter] key to start arch-chroot..."
 # Jump into installation
-arch-chroot /mnt /bin/bash <<'EOT'
 
-# Variables
-hostname=desktop
-username=admin
-password=password
-rootpassword=toor
+# Download chroot script
+curl -o /install.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/arch-chroot.sh
+chmod +x /install.sh
 
-# User config
-read -p "Press [Enter] key to start Europe Paris local time..."
-ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-read -p "Press [Enter] key to start hwclock..."
-hwclock --systohc
-read -p "Press [Enter] key to start Keymap..."
-echo "KEYMAP=fr" > /etc/vconsole.conf
-read -p "Press [Enter] key to start lang..."
-echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
-
-read -p "Press [Enter] key to start password & user ..."
-# User
-echo "$rootpassword" | passwd --stdin
-
-useradd -m -s /bin/bash ${username}
-echo "$password" | passwd $username --stdin
-
-usermod -aG wheel ${username}
-hostnamectl set-hostname ${hostname}
-
-read -p "Press [Enter] key to start download grub..."
-# Setup grub (TO TEST) & OS Prober = dual boot
-yes | pacman -S grub os-prober
-# yes | pacman -S os-prober # only os-prober to test systemd
-
-# mkinitcpio
-sed -i 's/HOOKS=(.*)/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
-
-read -p "Press [Enter] key to start install..."
-# grub
-grub-install --target=x86_64-efi --bootloader-id=ArchLinux --efi-directory=/boot --removable /dev/sda
-
-read -p "Press [Enter] key to start setDisk..."
-# Set Disk ID to LVM
-UUIDcrypt=$(blkid -o value -s UUID /dev/sda2)
-UUIDroot=$(blkid -o value -s UUID /dev/mapper/cryptlvm)
-sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"\(.*\)\"/GRUB_CMDLINE_DEFAULT=\"loglevel=3 quiet cryptdevice=UUID=${UUIDcrypt}:cryptlvm root=UUID=${UUIDroot}\"/" /etc/default/grub
-
-read -p "Press [Enter] key to start grub mkconfig..."
-# grub mkconfig
-grub-mkconfig -o /boot/grub/grub.cfg
-mkinitcpio -P
-
-read -p "Press [Enter] key to finish..."
-EOT
-
+arch-chroot /mnt /arch-chroot.sh
 
 
 # umount -R /mnt # Removed for TEST
