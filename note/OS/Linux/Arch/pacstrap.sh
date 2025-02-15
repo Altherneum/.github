fca77b092b56a82dfaba43aa30c314fbe0507c22
lx78WyY0J5 <9j2k37st@gmail.com>
2025-02-15 10:32:27 +0100

    new way to test chroot cmd

--- note/OS/Linux/Arch/pacstrap.sh
+++ note/OS/Linux/Arch/pacstrap.sh
@@ -13,4 +13,13 @@ chmod +x /arch-chroot-grub.sh
 # Download chroot script ²
 curl -o /arch-chroot-systemd.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/arch-chroot-systemd.sh
 chmod +x /arch-chroot-systemd.sh
-/arch-chroot-systemd.sh
+# /arch-chroot-systemd.sh
+
+# Test script
+curl -o /arch-chroot-systemd2-start.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/arch-chroot-systemd2-start.sh
+chmod +x /arch-chroot-systemd2-start.sh
+# /arch-chroot-systemd.sh
+
+curl -o /mnt/arch-chroot-systemd2.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/arch-chroot-systemd2.sh
+chmod +x /mnt/arch-chroot-systemd2.sh
+# /arch-chroot-systemd.sh
