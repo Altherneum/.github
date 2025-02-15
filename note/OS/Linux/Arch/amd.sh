a613cd63f80e5223474564c3b22663bf6e8f11a4
lx78WyY0J5 <9j2k37st@gmail.com>
2025-02-15 22:15:48 +0100

    Create amd.sh

---
+++ note/OS/Linux/Arch/amd.sh
@@ -0,0 +1,6 @@
+# https://www.reddit.com/r/archlinux/comments/17zgz7b/i_am_new_to_arch_and_linux_how_can_i_install_amd/
+# https://wiki.archlinux.org/title/AMDGPU
+
+# sudo pacman -S mesa vulkan-radeon libva-mesa-driver libva-utils # To test # From reddit # Cleaned of 32bits
+# sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils # To test # From reddit # Not cleaned of 32bits
+
