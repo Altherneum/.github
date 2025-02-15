2686fd71150a478277a12af94029960d711b7a9b
lx78WyY0J5 <9j2k37st@gmail.com>
2025-02-15 05:02:29 +0100

    Update to make final step manual

--- note/OS/Linux/Arch/pacstrap.sh
+++ note/OS/Linux/Arch/pacstrap.sh
@@ -12,4 +12,4 @@ chmod +x /arch-chroot-grub.sh
 # Download chroot script ²
 curl -o /arch-chroot-systemd.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/arch-chroot-systemd.sh
 chmod +x /arch-chroot-systemd.sh
-/arch-chroot-systemd.sh
+# /arch-chroot-systemd.sh
