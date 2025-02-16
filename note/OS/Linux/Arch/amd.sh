1537b497bc015de9ef8c53b9676c509b5e4c4064
lx78WyY0J5 <9j2k37st@gmail.com>
2025-02-16 07:04:16 +0100

    Update amd.sh

--- note/OS/Linux/Arch/amd.sh
+++ note/OS/Linux/Arch/amd.sh
@@ -1,1 +1,1 @@
-sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils lib32-systemd amdvlk lib32-amdvlk
+pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils lib32-systemd amdvlk lib32-amdvlk
