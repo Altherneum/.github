fd64c05ef619c9b35f88c8b526c332f6128fdd17
lx78WyY0J5 <9j2k37st@gmail.com>
2025-02-16 07:02:14 +0100

    Update amd.sh

--- note/OS/Linux/Arch/amd.sh
+++ note/OS/Linux/Arch/amd.sh
@@ -1,7 +1,2 @@
-# https://www.reddit.com/r/archlinux/comments/17zgz7b/i_am_new_to_arch_and_linux_how_can_i_install_amd/
-# https://wiki.archlinux.org/title/AMDGPU
+sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils lib32-systemd amdvlk lib32-amdvlk
 
-# sudo pacman -S mesa vulkan-radeon libva-mesa-driver libva-utils # To test # From reddit # Cleaned of 32bits
-# sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils # To test # From reddit # Not cleaned of 32bits
-
-# lib32-systemd vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk
