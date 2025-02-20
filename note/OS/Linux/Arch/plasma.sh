922e180aa1d47797b26f71f6ff51839ea9c2226f
lx78WyY0J5 <9j2k37st@gmail.com>
2025-02-20 17:58:04 +0100

    Update plasma.sh to add tiling manager & comment

--- note/OS/Linux/Arch/plasma.sh
+++ note/OS/Linux/Arch/plasma.sh
@@ -1,3 +1,12 @@
+# Plasma setup
 pacman -S plasma
-pacman -S plasma-meta
-pacman -S plasma-desktop
+# pacman -S plasma-meta # 
+# pacman -S plasma-desktop # Minimal configuration
+
+# Wayland
+## Stacking
+## Tiling
+pacman -S swayfx
+### Sway
+## Dynamic
+## 0ther
