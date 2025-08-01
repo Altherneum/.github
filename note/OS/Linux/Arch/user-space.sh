fe84f51a3c37621370fcd5ef2a6b918373d7a897
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-01 22:30:26 +0200

    Update user-space.sh

--- note/OS/Linux/Arch/user-space.sh
+++ note/OS/Linux/Arch/user-space.sh
@@ -4,3 +4,7 @@ pacman -S ufw openssh pipewire lib32-pipewire git vscodium github-desktop-bin br
 # Drive rclone
 # EpicGames
 # Ubisoft
+
+curl -o /plasma.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/plasma.sh
+chmod +x /plasma.sh
+/plasma.sh
