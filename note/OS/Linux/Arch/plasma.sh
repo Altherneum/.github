9fc22e49811e1f80c7056f7ca2f29817db64cc6d
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-02 07:01:56 +0200

    Update plasma.sh

--- note/OS/Linux/Arch/plasma.sh
+++ note/OS/Linux/Arch/plasma.sh
@@ -1,15 +1,1 @@
-# Plasma setup
-pacman -S plasma
-# pacman -S plasma-meta # 
-# pacman -S plasma-desktop # Minimal configuration
-
-# Wayland
-## https://wiki.archlinux.org/title/Wayland
-### https://www.fosskers.ca/en/blog/wayland
-## Tiling
-pacman -S swayfx # sway
-## Window manager
-pacman -S hyprland
-pacman -S labwc
-## Display manager
-pacman -S sddm
+yes | pacman -S plasma swayfx hyprland labwc sddm
