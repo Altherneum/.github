a81baa0790142f6184557cea98a7184555031c91
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-02 07:01:10 +0200

    Update amd.sh

--- note/OS/Linux/Arch/amd.sh
+++ note/OS/Linux/Arch/amd.sh
@@ -1,4 +1,4 @@
-pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils lib32-systemd amdvlk lib32-amdvlk
+yes | pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils lib32-systemd amdvlk lib32-amdvlk
 
 curl -o /user-space.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/user-space.sh
 chmod +x /user-space.sh
