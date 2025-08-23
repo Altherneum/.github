af37ff7abd0e6def4ce46c038177c04a6c134ec6
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-23 17:38:46 +0200

    UTF lang

--- note/OS/Linux/Arch/Archinstall/arch.sh
+++ note/OS/Linux/Arch/Archinstall/arch.sh
@@ -8,6 +8,7 @@ device="/dev/sda"
 localtime=/Europe/Paris
 langkey=fr
 fonttype=ter-v28b
+utflang=fr_FR.UTF-8
 
 # Timezone and Clock
 ln -sf /usr/share/zoneinfo$Timezone /etc/localtime
@@ -17,3 +18,7 @@ hwclock --systohc
 echo "KEYMAP=$langkey" > /etc/vconsole.conf
 echo "FONT=$fonttype" >> /etc/vconsole.conf
 
+# Locale
+echo "$utflang UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=$utflang" > /etc/locale.conf
