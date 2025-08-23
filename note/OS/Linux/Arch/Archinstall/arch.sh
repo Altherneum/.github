3db09de754ec7b772b7494fff0ffcfb3b8570aff
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-23 17:36:36 +0200

    add timezone & console settings

--- note/OS/Linux/Arch/Archinstall/arch.sh
+++ note/OS/Linux/Arch/Archinstall/arch.sh
@@ -5,4 +5,15 @@ username="arch"
 userpassword="arch"
 hostname="ARCH"
 device="/dev/sda"
+localtime=/Europe/Paris
+langkey=fr
+fonttype=ter-v28b
+
+# Timezone and Clock
+ln -sf /usr/share/zoneinfo$Timezone /etc/localtime
+hwclock --systohc
+
+# Console Settings
+echo "KEYMAP=$langkey" > /etc/vconsole.conf
+echo "FONT=$fonttype" >> /etc/vconsole.conf
 
