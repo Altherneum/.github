8e3a65777a7214a24739671724159e13d18d6df0
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-27 23:06:29 +0200

    update

--- note/OS/Linux/Arch/Archinstall/user-chroot.sh
+++ note/OS/Linux/Arch/Archinstall/user-chroot.sh
@@ -3,12 +3,14 @@ username=$(whoami)
 echo "Old home : $HOME"
 HOME=/home/$username
 echo "new home is : $HOME"
+userpassword="UserPass123"
 
 # Yay installation
-mkdir ~/git-clone && cd ~/git-clone && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
+mkdir ~/git-clone && cd ~/git-clone && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -s --noconfirm
+yaytar=$(find ~/git-clone/yay/ -name 'yay-*.pkg.tar.zst' | grep -v 'debug')
+echo $userpassword | sudo pacman -U $yaytar
 
 # swayfx
-userpassword="UserPass123"
 echo $userpassword | sudo -S yay -S --noconfirm --needed swayfx brave-bin pipewire-{jack,jack-dropin,alsa,pulse}
 
 ## Enable PipeWire
@@ -19,6 +21,10 @@ pactl info
 
 # Hyperland configuration
 mkdir -p ~/.config/hypr && cp /usr/share/hyprland/examples/hyprland.conf ~/.config/hypr/
+
+# Clean install
+echo $userpassword | sudo pacman -Scc --noconfirm
+echo $userpassword | sudo yay -Scc --noconfirm
 
 # Exit user arch-chroot
 echo "Exit user arch-chroot"
