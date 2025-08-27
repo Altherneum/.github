e535d32201f0d34f6dea28f4cd36de7c0638746c
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-27 19:25:22 +0200

    compact yay install

--- note/OS/Linux/Arch/Archinstall/user-chroot.sh
+++ note/OS/Linux/Arch/Archinstall/user-chroot.sh
@@ -8,24 +8,13 @@ echo "new home is : $HOME"
 mkdir ~/git-clone && cd ~/git-clone && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 
 # swayfx
-yay -S --noconfirm --needed swayfx 
+yay -S --noconfirm --needed swayfx brave-bin pipewire-{jack,jack-dropin,alsa,pulse} vscodium-bin github-desktop-bin steam
 
-# Brave browser
-yay -S --noconfirm --needed brave-bin
-
-# PipeWire audio
-yay -S --noconfirm --needed pipewire-{jack,jack-dropin,alsa,pulse}
 ## Enable PipeWire
 systemctl --user enable pipewire pipewire-pulse wireplumber
 # systemctl --user start pipewire pipewire-pulse wireplumber # No need to start it will do it on reboot
 ## Show if PipeWire is running
 pactl info
-
-# Dev
-yay -S --noconfirm --needed vscodium-bin github-desktop-bin
-
-# Steam
-yay -S --noconfirm --needed steam
 
 # Hyperland configuration
 mkdir -p ~/.config/hypr && cp /usr/share/hyprland/examples/hyprland.conf ~/.config/hypr/
