67ab6a755d0edb8a3e60d5316fed5208ad02367b
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-27 11:12:03 +0200

    Update arch.sh

--- note/OS/Linux/Arch/Archinstall/arch.sh
+++ note/OS/Linux/Arch/Archinstall/arch.sh
@@ -61,10 +61,5 @@ curl -o /mnt/chroot.sh https://raw.githubusercontent.com/Altherneum/.github/refs
 chmod +x /mnt/chroot.sh
 arch-chroot /mnt /chroot.sh
 
-# User software installation
-curl -o /mnt/user-chroot.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archinstall/user-chroot.sh
-chmod +x /mnt/user-chroot.sh
-arch-chroot -u $username /mnt /user-chroot.sh
-
 echo "Remove the CD/USB ISO of arch"
 echo "Reboot into your new installation of Arch"
