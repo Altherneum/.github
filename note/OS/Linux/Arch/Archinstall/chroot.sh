4760456c230aea7c5af3187840773103a97f65dc
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-27 10:16:04 +0200

    Update chroot.sh

--- note/OS/Linux/Arch/Archinstall/chroot.sh
+++ note/OS/Linux/Arch/Archinstall/chroot.sh
@@ -108,8 +108,3 @@ bootctl list
 echo "Installation and basic configuration complete. exiting chroot"
 exit
 echo "exited"
-
-# User software installation
-curl -o /mnt/user-chroot.sh https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archinstall/user-chroot.sh
-chmod +x /mnt/user-chroot.sh
-arch-chroot -u $username /mnt /user-chroot.sh
