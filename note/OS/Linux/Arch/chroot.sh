08959dd14d9f8c02a5a1bc7ca9e25142ebd888af
lx78WyY0J5 <9j2k37st@gmail.com>
2025-09-02 23:49:35 +0200

    networkmanager added

--- note/OS/Linux/Arch/chroot.sh
+++ note/OS/Linux/Arch/chroot.sh
@@ -51,7 +51,7 @@ sed -iz 's/#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist/\[multilib\]\nI
 # Installing softwares
 pacman -Syu --noconfirm
 ## OS
-pacman -S --needed --noconfirm hyprland sddm wayland hyprlock hypridle waybar rofi
+pacman -S --needed --noconfirm hyprland sddm wayland hyprlock hypridle waybar rofi networkmanager
 ### Enable SDDM
 systemctl enable sddm
 ### Sound system
