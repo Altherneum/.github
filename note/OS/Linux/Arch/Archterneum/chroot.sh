098a4e5d4ad20d8299cdf70bbf81b8e9565b2abf
lx78WyY0J5 <9j2k37st@gmail.com>
2025-09-07 20:21:09 +0200

    Update chroot.sh

--- note/OS/Linux/Arch/Archterneum/chroot.sh
+++ note/OS/Linux/Arch/Archterneum/chroot.sh
@@ -59,7 +59,7 @@ pacman -S --needed --noconfirm obs-studio discord
 ## Steam
 pacman -S --needed --noconfirm steam
 ## System
-pacman -S --needed --noconfirm alacritty nautilus
+pacman -S --needed --noconfirm xfce4 nautilus
 pacman -S --needed --noconfirm git ufw keepassxc go
 ### Other software
 pacman -S --needed --noconfirm btop fastfetch htop
