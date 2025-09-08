77fa5d50db08433296bb2d7bd4a5680f11555801
lx78WyY0J5 <9j2k37st@gmail.com>
2025-09-08 08:14:21 +0200

    fix issue that was breaking steam install

--- note/OS/Linux/Arch/Archterneum/chroot.sh
+++ note/OS/Linux/Arch/Archterneum/chroot.sh
@@ -44,7 +44,7 @@ sed -i 's/^#Color/Color/' /etc/pacman.conf
 ## Add pacman Parallel Downloads
 sed -i 's/^#\?ParallelDownloads.*/ParallelDownloads = 1/' /etc/pacman.conf
 ## Add multilib pacman (for steam)
-sed -iz 's/#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist/\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist/' /etc/pacman.conf
+sed -z 's/#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist/\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist/' -i /etc/pacman.conf
 
 # Installing softwares
 pacman -Syu --noconfirm
