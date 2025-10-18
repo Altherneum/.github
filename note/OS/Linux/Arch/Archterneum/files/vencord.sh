e7663827afb0007699b759d25e99b018573084d9
lx78WyY0J5 <9j2k37st@gmail.com>
2025-10-18 18:32:38 +0200

    add sudo to script

--- note/OS/Linux/Arch/Archterneum/files/vencord.sh
+++ note/OS/Linux/Arch/Archterneum/files/vencord.sh
@@ -1,9 +1,9 @@
 username=$1
 
-pacman -S --needed --noconfirm discord
-curl -o /home/$username/vencord -L https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli-linux
-chmod +x /home/$username/vencord
+sudo pacman -S --needed --noconfirm discord
+sudo curl -o /home/$username/vencord -L https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli-linux
+sudo chmod +x /home/$username/vencord
 sudo pkexec env "$@" "SUDO_USER=$(whoami)" "/home/$username/vencord" --install -location /opt/discord
-rm -f /home/$username/vencord
-curl -o /home/$username/.config/Vencord/themes/transparent.css https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archterneum/files/discord.css
-curl -o /home/$username/.config/Vencord/settings/settings.json https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archterneum/files/discord.json
+sudo rm -f /home/$username/vencord
+sudo curl -o /home/$username/.config/Vencord/themes/transparent.css https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archterneum/files/discord.css
+sudo curl -o /home/$username/.config/Vencord/settings/settings.json https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archterneum/files/discord.json
