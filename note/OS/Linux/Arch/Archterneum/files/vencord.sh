66beeda7063c5e72bf886ad0956df9efc708cdf8
lx78WyY0J5 <9j2k37st@gmail.com>
2025-10-18 20:00:22 +0200

    discord pacman hook to setup vencord at each update

--- note/OS/Linux/Arch/Archterneum/files/vencord.sh
+++ note/OS/Linux/Arch/Archterneum/files/vencord.sh
@@ -1,9 +1,8 @@
-username=$1
+username=$(logname)
 
-sudo pacman -S --needed --noconfirm discord
 sudo curl -o /home/$username/vencord -L https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli-linux
 sudo chmod +x /home/$username/vencord
-sudo pkexec env "$@" "SUDO_USER=$(whoami)" "/home/$username/vencord" --install -location /opt/discord
+su -c 'sudo env "$@" "SUDO_USER=$(whoami)" "/home/'$username'/vencord" --install -location /opt/discord' $username
 sudo rm -f /home/$username/vencord
 sudo curl -o /home/$username/.config/Vencord/themes/transparent.css https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archterneum/files/discord.css
 sudo curl -o /home/$username/.config/Vencord/settings/settings.json https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archterneum/files/discord.json
