4bdefb501deed65ecfc714006430abaf61c77541
lx78WyY0J5 <9j2k37st@gmail.com>
2025-11-27 18:57:08 +0100

    update vencord script to create folders

--- note/OS/Linux/Arch/Archterneum/files/vencord.sh
+++ note/OS/Linux/Arch/Archterneum/files/vencord.sh
@@ -1,8 +1,18 @@
 username=$(logname)
 
+## Downloading Vencord
+echo "Downloading Vencord"
 sudo curl -o /home/$username/vencord -L https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli-linux
 sudo chmod +x /home/$username/vencord
 su -c 'sudo env "$@" "SUDO_USER=$(whoami)" "/home/'$username'/vencord" --install -location /opt/discord' $username
 sudo rm -f /home/$username/vencord
+
+## ~/.config/Vencord/themes/
+echo "Creating user ~/.config/Vencord/themes/"
+sudo -u $username mkdir -p /home/$username/.config/Vencord/themes/
 sudo curl -o /home/$username/.config/Vencord/themes/transparent.css https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archterneum/files/discord.css
+
+## ~/.config/Vencord/settings/
+echo "Creating user ~/.config/Vencord/settings/"
+sudo -u $username mkdir -p /home/$username/.config/Vencord/settings/
 sudo curl -o /home/$username/.config/Vencord/settings/settings.json https://raw.githubusercontent.com/Altherneum/.github/refs/heads/main/note/OS/Linux/Arch/Archterneum/files/discord.json
