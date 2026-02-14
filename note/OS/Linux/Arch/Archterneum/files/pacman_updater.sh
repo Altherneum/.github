f999340b29fb6ec526a49229e6174945a28614d8
lx78WyY0J5 <9j2k37st@gmail.com>
2026-02-14 21:56:06 +0100

    update keyring & keys before updating

--- note/OS/Linux/Arch/Archterneum/files/pacman_updater.sh
+++ note/OS/Linux/Arch/Archterneum/files/pacman_updater.sh
@@ -1,11 +1,20 @@
+# Refresh arch keyring & keys
+sudo pacman -Syy ; sudo pacman -S archlinux-keyring --noconfirm ; sudo pacman --refresh-keys || sudo pacman-key --refresh-keys
+
+# Update all pacman's repo
 sudo pacman -Syuu
+
+# Update all yay's repo
 yay -Syuu
 
+# Update waybar
 sudo pkill -SIGRTMIN+8 waybar
 
+# Send message that terminal close in 15s
 echo 
 echo 🛠️⚙️ 🔚
 echo
 echo 🕒️💤 15s ➡️ 🗑️ 
 
+# Sleep 15s
 sleep 15
