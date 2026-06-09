f1b9e8f909b6e6ecd416c4a29a342c54c09ed288
lx78WyY0J5 <9j2k37st@gmail.com>
2026-06-09 04:20:47 +0200

    add git PR extension

--- note/OS/Termux/code-server.sh
+++ note/OS/Termux/code-server.sh
@@ -4,6 +4,7 @@ pkg install tur-repo
 apt update --fix-missing
 
 pkg install git
+
 git clone https://github.com/Altherneum/.github
 git clone https://github.com/Altherneum/plugin
 git clone https://github.com/Altherneum/server
@@ -14,5 +15,7 @@ git config --global user.email "9j2k37st@gmail.com"
 git config --global user.name "lx78WyY0J5"
 
 pkg install code-server
-code-server --auth none
 
+code-server --install-extension GitHub.vscode-pull-request-github
+
+code-server --auth none
