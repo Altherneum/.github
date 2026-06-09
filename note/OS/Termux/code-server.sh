8d02972f810346895b4eabf34aa3e3c3d2cc8365
lx78WyY0J5 <9j2k37st@gmail.com>
2026-06-09 04:11:35 +0200

    add git config

--- note/OS/Termux/code-server.sh
+++ note/OS/Termux/code-server.sh
@@ -10,5 +10,9 @@ git clone https://github.com/Altherneum/server
 git clone https://github.com/Altherneum/bot
 git clone https://github.com/Altherneum/altherneum.github.io
 
+git config --global user.email "9j2k37st@gmail.com"
+git config --global user.name "lx78WyY0J5"
+
 pkg install code-server
 code-server --auth none
+
