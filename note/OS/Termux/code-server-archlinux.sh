6bb89977e10e8e7cc5a1b43435663d2693f2642c
lx78WyY0J5 <9j2k37st@gmail.com>
2026-08-20 10:58:22 +0200

    Create code-server-archlinux.sh

---
+++ note/OS/Termux/code-server-archlinux.sh
@@ -0,0 +1,21 @@
+pkg update && pkg upgrade
+pkg install tur-repo
+
+apt update --fix-missing
+
+pkg install git
+
+git clone https://github.com/Altherneum/.github
+git clone https://github.com/Altherneum/plugin
+git clone https://github.com/Altherneum/server
+git clone https://github.com/Altherneum/bot
+git clone https://github.com/Altherneum/altherneum.github.io
+
+git config --global user.email "9j2k37st@gmail.com"
+git config --global user.name "lx78WyY0J5"
+
+pkg install code-server
+
+code-server --install-extension GitHub.vscode-pull-request-github
+
+code-server --auth none
