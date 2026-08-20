3bcd44ef14098830e7c58086b9161745e5d60720
lx78WyY0J5 <9j2k37st@gmail.com>
2026-08-20 10:58:45 +0200

    Update arch.sh

--- note/OS/Termux/arch.sh
+++ note/OS/Termux/arch.sh
@@ -1,10 +1,6 @@
 pkg update
 pkg install proot-distro
 
-# Avec Arch Linux
 proot-distro install archlinux
 proot-distro login archlinux
 
-# Avec Ubuntu (Pour ARM car ArchLinux n'est pas disponible)
-proot-distro install ubuntu
-proot-distro login ubuntu
